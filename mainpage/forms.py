e7a57231191da6bb673f60c4c7a712e6ee3e9c0a
islam <islamveto12@gmail.com>
2022-05-03 17:19:14 +0100

    delete importations

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -2,7 +2,7 @@ from asyncore import read
 from cProfile import label
 from dataclasses import field, fields
 from pyexpat import model
-from socket import fromshare
+
 from tkinter import Widget
 from django import forms
 from .models import UserPayementStat, Account
