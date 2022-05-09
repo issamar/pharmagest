2a1e5bdd2a4485f4c2a4cc50e23631efe875ce72
islam <islamveto12@gmail.com>
2022-05-09 22:29:34 +0100

    complete email verification

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,6 @@
 from dataclasses import fields
 from sys import flags
+from tokenize import generate_tokens
 from urllib import request
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
@@ -28,36 +29,53 @@ def RegisterPage(request):
             if form.is_valid():
                 user_name = request.POST['username']
                 email = request.POST['email']
-                form.save()
-                User.objects.filter(username = user_name).update(is_active = False)
-                user_pk = User.objects.filter(username =user_name ).values_list('pk', flat=True)
-                print(user_pk, flush=True)
-                user = User.objects.get(pk=user_pk[0])
-                domaine = get_current_site(request).domain
-                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
-                link = reverse('activate', kwargs={'uidb64':uidb64, 'token': token_generator.make_token(user)})
-                activation_url = 'https://' + domaine+link
-                email_subject = 'Activez votre compte'
-                email_body = 'Cher(e) ' + user.username + "\n Vous avez enregistré sur le site de OffGest Pour continuer l'enregistrement veuillez cliquey sur le lien ci-dessous\n"  + activation_url
-                rec_email = 'pharmagest.22@gmail.com'
-                email = EmailMessage(
-                    email_subject,
-                    email_body,
-                    rec_email,
-                    [email],
-                )
-                email.send(fail_silently = False)  
-                messages.success(request,'Vous avez enregistré avec succés')
-                return redirect('main-page')
-    return render(request,'register.html', {'form' : form,})
+                all_emails = User.objects.values_list('email', flat=True)
+                if email not in all_emails:
+                    form.save()
+                    User.objects.filter(username = user_name).update(is_active = False)
+                    user_pk = User.objects.filter(username =user_name ).values_list('pk', flat=True)
+                
+                    user = User.objects.get(pk=user_pk[0])
+                    domaine = get_current_site(request).domain
+                    uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+                    link = reverse('activate', kwargs={'uidb64':uidb64, 'token': token_generator.make_token(user)})
+                    activation_url = 'http://' + domaine+link
+                    email_subject = 'Activez votre compte'
+                    email_body = 'Cher(e) ' + user.username + "\n Vous avez enregistré sur le site de OffGest Pour continuer l'enregistrement veuillez cliquey sur le lien ci-dessous\n"  + activation_url
+                    rec_email = 'pharmagest.22@gmail.com'
+                    email = EmailMessage(
+                        email_subject,
+                        email_body,
+                        rec_email,
+                        [email],
+                    )
+                    email.send(fail_silently = False)  
+                    messages.success(request,'Vous avez enregistré avec succés')
+                    return redirect('main-page')
+                else:
+                    messages.warning(request,'Email deja utilisé')
+        return render(request,'register.html', {'form' : form,})
 
 
 class verification(View):
     def get(self,request,uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except Exception as e:
+            user = None
+        if user and token_generator.check_token(user,token):
+            user.is_active = True
+            user.save()
         return redirect('main-page')
 
 
 def MainPage(request):
+    userrname = request.user
+    print(userrname, flush=True)
+    get_userrname_id = User.objects.filter(username = userrname).values_list('id', flat=True)
+    get_userrname = User.objects.get(pk=get_userrname_id[0])
+    print(get_userrname.date_joined, flush=True)
     if request.method == 'POST':
         user_name = request.POST['username']
         pass_word = request.POST['password']
@@ -90,3 +108,4 @@ def UserProofView(request):
             form.save()
             return redirect('main-page')
     return render (request, 'user_prof.html',{'form' : form})
+
