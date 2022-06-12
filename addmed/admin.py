1aa6f4f9c795114e0ad600b32395a23e938aaf2a
islam <islamveto12@gmail.com>
2022-06-12 14:07:23 +0100

    searchbox adminarea

--- addmed/admin.py
+++ addmed/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Addart, Addmed
 # Register your models here.
-admin.site.register(Addmed)
-admin.site.register(Addart)
+class MedSearch(admin.ModelAdmin):
+    search_fields = ('name',)
+
+class ArtSearch(admin.ModelAdmin):
+    search_fields = ('full_name',)
+admin.site.register(Addmed, MedSearch)
+admin.site.register(Addart, ArtSearch)
