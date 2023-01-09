5e328987821bec131b2b884a5f32926fcb278cb6
issamar <islamveto12@gmail.com>
2023-01-09 18:38:52 +0100

    add searching by date in lab

--- lab/views.py
+++ lab/views.py
@@ -12,18 +12,18 @@ from .models import Patients, Parameters, Labo
 # Create your views here.
 @login_required(login_url='main-page')
 
-def display_detail(request):
-    current_date = date.today()
-    all_labs = Labo.objects.filter(dt=current_date)
+def display_detail(request, searched_date = date.today() ):
+    print(request, flush=True)
+    all_labs = Labo.objects.filter(dt=searched_date)
     patients = Patients.objects.all()
     parameters= Parameters.objects.all()
-    print(current_date, flush=True)
-    prele_count = Labo.objects.filter(dt=current_date).count()
-    total_payement = Labo.objects.filter(dt=current_date).aggregate(Sum('pay'))
-    total_rest = Labo.objects.filter(dt=current_date).aggregate(Sum('rest'))
-    hba1c_free = Labo.objects.filter(dt=current_date).filter(hba1c = 'True').count()
+    
+    prele_count = Labo.objects.filter(dt=searched_date).count()
+    total_payement = Labo.objects.filter(dt=searched_date).aggregate(Sum('pay'))
+    total_rest = Labo.objects.filter(dt=searched_date).aggregate(Sum('rest'))
+    hba1c_free = Labo.objects.filter(dt=searched_date).filter(hba1c = 'True').count()
     return render(request, 'addp.html', {'patients' : patients, 'parameters' : parameters, 'all_labs' : all_labs,'prele_count':prele_count,
-                    'total_payement':total_payement,'total_rest':total_rest,'hba1c_free':hba1c_free, 'current_date':current_date})
+                    'total_payement':total_payement,'total_rest':total_rest,'hba1c_free':hba1c_free, 'searched_date':searched_date})
 
 @login_required(login_url='main-page')
 def addP(request):
@@ -92,11 +92,25 @@ def editrest(request, pk):
     return render(request, 'edit-rest.html',{'getPatient' : getPatient})
 @login_required(login_url='main-page')
 def searchP(request):
-    print(request.POST, flush=True)
-    searched = Labo.objects.filter(p_name= request.POST['p_name'])
+    print("2",request, flush=True)
+    patient_name = request.POST['p_name']
+    searched_date = request.POST['dt']
+    if patient_name and searched_date:
+        searched = Labo.objects.filter(p_name= request.POST['p_name'], dt= searched_date)
+    elif patient_name:
+        searched = Labo.objects.filter(p_name= request.POST['p_name'])
+    elif searched_date:
+        searched = Labo.objects.filter( dt= searched_date)
     patients = Patients.objects.all()
     parameters= Parameters.objects.all()
     d_patient = Labo.objects.values_list('p_name')
+    prele_count = Labo.objects.filter(dt=searched_date).count()
+    total_payement = Labo.objects.filter(dt=searched_date).aggregate(Sum('pay'))
+    total_rest = Labo.objects.filter(dt=searched_date).aggregate(Sum('rest'))
+    hba1c_free = Labo.objects.filter(dt=searched_date).filter(hba1c = 'True').count()
+
+    
     
 
-    return render(request,'addp.html',{'searched':searched,'patients':patients,'parameters':parameters,'d_patient':d_patient})
+    return render(request,'addp.html',{'searched':searched,'patients':patients,'parameters':parameters,'d_patient':d_patient,
+                    'prele_count':prele_count,'total_payement':total_payement,'total_rest':total_rest,'hba1c_free':hba1c_free, 'searched_date':searched_date})
